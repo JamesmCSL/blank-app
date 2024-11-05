32177340430de1159c1350eb9e309edd932981ce
James Miller <james.miller@cloudsecure.ltd>
2024-11-05 17:34:55 +0000

    Added metal oxide selector

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,27 +3,24 @@ import pandas as pd
 from ucimlrepo import fetch_ucirepo
 from datetime import datetime as dt
 
-# Get data
+st.set_page_config(layout="wide")
 
+# Get data
 air_quality = fetch_ucirepo(id=360)
 df = pd.DataFrame(air_quality.data.features)
+gases = ['CO(GT)', 'NO2(GT)', 'C6H6(GT)', 'NOx(GT)']
 
 # Clean data
 dateTime = pd.to_datetime(df['Date'] + ' ' + df['Time'])
 df.insert(0, 'DateTime', dateTime)
-df['Date'] = pd.to_datetime(df['DateTime'])
-df['Date'] = pd.to_datetime(df['Time'])
+df['Date'] = pd.to_datetime(df['DateTime'], format='%Y-%m-%d %H-%M-%S')
 df.sort_values('DateTime', inplace=True)
+df = df[['DateTime', 'T', 'RH'] + gases]
+df.rename(columns={'CO(GT)': 'CO', 'NO2(GT)': 'NO2', 'C6H6(GT)': 'C6H6', 'NOx(GT)':'NOx'})
 
 # Get rid of columns missing values (will improve later...)
-df = df[~df['T'].isin([-200])]
-df = df[~df['CO(GT)'].isin([-200])]
-df = df[~df['NO2(GT)'].isin([-200])]
-df = df[~df['RH'].isin([-200])]
-
-# Data Selection
-temps = df.groupby(df['Date'].dt.floor('D'))['T'].mean()
-humidity = df.groupby(df['Date'].dt.floor('D'))['RH'].mean()
+for col in df.columns:
+    df = df[~df[col].isin([-200])]
 
 # App
 st.title("CloudSecure Ltd Data Dashboard")
@@ -43,12 +40,26 @@ from_date, to_date = st.slider(
     label='Please select a date range:'
     )
 
+selected_gases = st.multiselect(
+    "Which metal oxide concentrations would you like to measure?",
+    ['CO', 'NO2', 'C6H6', 'NOx'],
+    ['CO', 'NO2', 'C6H6', 'NOx']
+)
+
 st.divider()
 
 filtered_df = df[(df.DateTime <= to_date) & (df.DateTime >= from_date)]
+filtered_df = df[['DateTime', 'T', 'RH']]
+
+# df_gases = df[[selected_gases].append('DateTime')]
+
+# st.write(f"{selected_gases}")
 
 temps = filtered_df.groupby(df['DateTime'].dt.floor('D'))['T'].mean()
 humidity = filtered_df.groupby(df['DateTime'].dt.floor('D'))['RH'].mean()
 
-st.line_chart(temps, x_label='Date', y_label='Temperature (°C)', color='#2EA3D7')
-st.line_chart(humidity, x_label='Date', y_label='% Relative Humidity')
+col1, col2 = st.columns(2)
+with col1:
+    st.line_chart(temps, y_label='Temperature (°C)', color='#2EA3D7')
+with col2:
+    st.line_chart(humidity, y_label='% Relative Humidity')
